a832f1b687f957eaee639a345abfa7532f528ac6
Fortune-Essien <97860137+Fortune-Essien@users.noreply.github.com>
2022-02-11 04:57:04 +0100

    Update Portfolio_Project (COVID_Data).sql

--- Portfolio_Project (COVID_Data).sql
+++ Portfolio_Project (COVID_Data).sql
@@ -3,9 +3,11 @@
 
 
 -- In this Query, i explore the data to find the following
+-- Number of covid tests carried out
 -- Population that has gotten infected with COVID
 -- deaths in Population due to COVID
 -- Population that has gotten vaccinated against COVID
+-- Top 20 countries with (1) Infection count (2) Death Count (3) Vaccination count
 
 
 
@@ -31,14 +33,14 @@ order by 1
 select *
 from PortfolioProject..CovidDeaths
 
-select continent, location, population, --1 begin by selectin everything, then columns of interest
-max(cast(total_deaths as int)) as TotalDeaths, --8
-(max(cast(total_deaths as int))/population)*100 as DeathRate --9
-from PortfolioProject..CovidDeaths --2
-where continent is not null --5
---and continent = 'africa' --10
-group by location, continent, population --3
-order by 1 --4
+select continent, location, population,
+max(cast(total_deaths as int)) as TotalDeaths,
+(max(cast(total_deaths as int))/population)*100 as DeathRate
+from PortfolioProject..CovidDeaths
+where continent is not null
+--and continent = 'africa'
+group by location, continent, population
+order by 1
 
 
 
@@ -66,18 +68,19 @@ group by death.location, death.continent, death.population
 
 
 
--- This query finds the Total infections, Total deaths, and Total Vaccination counts for each continent
+-- This query finds the Total infections, Total deaths, and Total Vaccination counts across the world
 
-select dea.location, dea.population,
-sum(dea.new_cases) as TotalCases,
-sum(cast(dea.new_deaths as int)) as TotalDeaths,
-sum(convert(bigint,vac.new_vaccinations)) as TotaVaccinations
+select dea.continent, dea.location,
+sum(convert(bigint,vax.new_tests)) as totaltests,
+sum(dea.new_cases) as totalcases,
+sum(cast(dea.new_deaths as bigint)) as totaldeaths,
+sum(cast(vax.new_vaccinations as bigint)) as totalvaccinations
 from PortfolioProject..CovidDeaths as dea
-join PortfolioProject..CovidVaccinations as vac
-	on dea.location = vac.location
-	and dea.date = vac.date
-where dea.continent is null
---and dea.location = 'africa'
+join PortfolioProject..CovidVaccinations as vax
+	on dea.location = vax.location
+	and dea.date = vax.date
+where dea.continent is not null
+--and dea.continent = 'africa'
 and dea.location <> 'European union'
 and dea.location <> 'high income'
 and dea.location <> 'International'
@@ -85,4 +88,67 @@ and dea.location <> 'Low income'
 and dea.location <> 'Lower middle income'
 and dea.location <> 'Upper middle income'
 and dea.location <> 'World'
-group by dea.location, dea.population
+group by dea.continent, dea.location
+
+
+
+-- This query finds the top 20 countries with infection cases
+
+select top 20 dea.continent, dea.location,
+sum(dea.new_cases) as totalcases
+from PortfolioProject..CovidDeaths as dea
+join PortfolioProject..CovidVaccinations as vax
+	on dea.location = vax.location
+	and dea.date = vax.date
+where dea.continent is not null
+--and dea.continent = 'africa'
+and dea.location <> 'European union'
+and dea.location <> 'high income'
+and dea.location <> 'International'
+and dea.location <> 'Low income'
+and dea.location <> 'Lower middle income'
+and dea.location <> 'Upper middle income'
+and dea.location <> 'World'
+group by dea.continent, dea.location
+order by 3 desc
+
+-- This query finds the top 20 countries with Death Counts
+
+select top 20 dea.continent, dea.location,
+sum(cast(dea.new_deaths as bigint)) as totaldeaths
+from PortfolioProject..CovidDeaths as dea
+join PortfolioProject..CovidVaccinations as vax
+	on dea.location = vax.location
+	and dea.date = vax.date
+where dea.continent is not null
+--and dea.continent = 'africa'
+and dea.location <> 'European union'
+and dea.location <> 'high income'
+and dea.location <> 'International'
+and dea.location <> 'Low income'
+and dea.location <> 'Lower middle income'
+and dea.location <> 'Upper middle income'
+and dea.location <> 'World'
+group by dea.continent, dea.location
+order by 3 desc
+
+
+-- This query finds the top 20 countries with Vaccination counts
+
+select top 20 dea.continent, dea.location,
+sum(cast(vax.new_vaccinations as bigint)) as totalvaccinations
+from PortfolioProject..CovidDeaths as dea
+join PortfolioProject..CovidVaccinations as vax
+	on dea.location = vax.location
+	and dea.date = vax.date
+where dea.continent is not null
+--and dea.continent = 'africa'
+and dea.location <> 'European union'
+and dea.location <> 'high income'
+and dea.location <> 'International'
+and dea.location <> 'Low income'
+and dea.location <> 'Lower middle income'
+and dea.location <> 'Upper middle income'
+and dea.location <> 'World'
+group by dea.continent, dea.location
+order by 3 desc
